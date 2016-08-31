bcc9997690c74fd2aa65cc6ffab2cd4f111fddef
EQwon <bluetwintail@naver.com>
2016-09-01 04:29:26 +0900

    포기했습니다....

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float PlayerMovingSpeed;
     public bool TeleportMode;
 
     void Start()
@@ -14,130 +13,49 @@ public class PlayerController : MonoBehaviour
 	void Update ()
 	{
         MovingKeyInput();
+
+        if (TeleportMode)
+            TeleportAlgorithm();
 	}
+
+    void TeleportAlgorithm()
+    {
+        TeleportRight(-4, 4.05f);
+        TeleportRight(3, 4.05f);
+
+        TeleportLeft(0, 4.05f);
+        TeleportLeft(7, 4.05f);
+    }
 
 	void MovingKeyInput ()
 	{
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(PlayerMovingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + 0.05f, GetComponent<Transform>().position.y, 0);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-PlayerMovingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x - 0.05f, GetComponent<Transform>().position.y, 0);
         }
         else
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y, 0);
         }
 	}
 
-    void OnTriggerEnter2D(Collider2D Coll)
+    void TeleportRight(float StartTeleportPos, float TeleportDistance)
     {
-        GameObject Object = Coll.gameObject;
-        float ObjectXPos = Object.GetComponent<Transform>().position.x;
-        float ObjectYPos = Object.GetComponent<Transform>().position.y;
-
-        if (TeleportMode)
+        if ((Mathf.Abs(GetComponent<Transform>().position.x - StartTeleportPos) < 0.01f) && Input.GetKey(KeyCode.RightArrow))
         {
-            if (Object.tag == "H_Arcade")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 3.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 3.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_78")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 3.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 3.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Bell")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 5.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 5.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Cherry")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Gahoon")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Gate")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Kite")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_LampPaper")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_LampStone")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Pot")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Ramen")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_SakuraFlower")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 5.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 5.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_SakuraTrunk")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
-            }
-            else if (Object.tag == "H_Vending")
-            {
-                if (ObjectXPos > transform.position.x)
-                    transform.position = new Vector2(transform.position.x + 3.5f, transform.position.y);
-                else if (ObjectXPos < transform.position.x)
-                    transform.position = new Vector2(transform.position.x - 3.5f, transform.position.y);
-            }
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + TeleportDistance, GetComponent<Transform>().position.y, 0);
+        }
+    }
+
+    void TeleportLeft(float StartTeleportPos, float TeleportDistance)
+    {
+        if ((Mathf.Abs(GetComponent<Transform>().position.x - StartTeleportPos) < 0.01f) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x - TeleportDistance, GetComponent<Transform>().position.y, 0);
         }
     }
 }
