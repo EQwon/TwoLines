2ee0d29a8b9fcc0ceb4cecf1d203690691c0ae82
EQwon <bluetwintail@naver.com>
2016-09-01 01:57:36 +0900

    오브젝트 감지하도록 함

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,4 +30,21 @@ public class PlayerController : MonoBehaviour
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
         }
 	}
+
+    void OnTriggerEnter2D(Collider2D Coll)
+    {
+        if (Coll.gameObject.tag == "Object")
+        {
+            GameObject Object = Coll.gameObject;
+            float ObjectXSize = Object.GetComponent<BoxCollider2D>().size.x;
+            float ObjectYSize = Object.GetComponent<BoxCollider2D>().size.y;
+            float ObjectXPos = Object.GetComponent<Transform>().position.x;
+            float ObjectYPos = Object.GetComponent<Transform>().position.y;
+
+            if (ObjectXPos > transform.position.x)
+                Debug.Log("Left");
+            else if (ObjectXPos < transform.position.x)
+                Debug.Log("Right");
+        }
+    }
 }
