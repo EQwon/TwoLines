6649bfd2be1169d143f4afc1c12922295f80ab79
EQwon <bluetwintail@naver.com>
2016-09-01 03:14:30 +0900

    하나무라 스테이지의 모든 프리팹 제작

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public float PlayerMovingSpeed;
+    public bool TeleportMode;
 
     void Start()
     {
-        
+        TeleportMode = true;
     }
 
 	void Update ()
@@ -33,18 +34,110 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D Coll)
     {
-        if (Coll.gameObject.tag == "Object")
-        {
-            GameObject Object = Coll.gameObject;
-            float ObjectXSize = Object.GetComponent<BoxCollider2D>().size.x;
-            float ObjectYSize = Object.GetComponent<BoxCollider2D>().size.y;
-            float ObjectXPos = Object.GetComponent<Transform>().position.x;
-            float ObjectYPos = Object.GetComponent<Transform>().position.y;
+        GameObject Object = Coll.gameObject;
+        float ObjectXPos = Object.GetComponent<Transform>().position.x;
+        float ObjectYPos = Object.GetComponent<Transform>().position.y;
 
-            if (ObjectXPos > transform.position.x)
-                Debug.Log("Left");
-            else if (ObjectXPos < transform.position.x)
-                Debug.Log("Right");
+        if (TeleportMode)
+        {
+            if (Object.tag == "H_Arcade")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 3.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 3.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_78")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 3.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 3.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Bell")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 5.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 5.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Cherry")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Gahoon")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Gate")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Kite")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_LampPaper")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_LampStone")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Pot")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 2.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 2.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Ramen")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_SakuraFlower")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 5.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 5.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_SakuraTrunk")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 1.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 1.5f, transform.position.y);
+            }
+            else if (Object.tag == "H_Vending")
+            {
+                if (ObjectXPos > transform.position.x)
+                    transform.position = new Vector2(transform.position.x + 3.5f, transform.position.y);
+                else if (ObjectXPos < transform.position.x)
+                    transform.position = new Vector2(transform.position.x - 3.5f, transform.position.y);
+            }
         }
     }
 }
